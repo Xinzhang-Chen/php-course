3cdcd49f7e90e7c0e101e3a4ee1165a4aaac2e95
Johnson-GitHub-New <64173435+Johnson-GitHub-New@users.noreply.github.com>
2023-06-01 13:17:13 +1000

    Upload

---
+++ 01_output.php
@@ -0,0 +1,19 @@
+
+<?php 
+  // Echo - Output strings, numbers, html, etc
+  echo "This is the first line.<br/>";
+  echo "This is the second line.";
+
+  // print - Similar to echo, but a bit slower
+  print "This is the first line.<br/>";
+
+  // print_r - Gives a bit more info. Can be used to print arrays
+  print_r([1,2,3]);
+  echo "<br/>";
+
+  // var_dump - Even more info like data type and length
+  var_dump('Hello');
+  echo "<br/>";
+  var_dump([1, 2, 3]);
+
+?>
